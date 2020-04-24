0dadd0eb9b1b2b9587333543028c6d3b728cb1ea
William Odell <wodell@liveperson.com>
2020-04-24 15:14:18 +0100

    Develop changes 1

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 console.log("This line will remain the same in all versions");
 
-console.log("This line will change in every version", 1);
-
-console.log("This line will be deleted in one version");
+console.log("This line will change in every version", "develop1");
