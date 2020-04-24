2798891f08ca22add9c3c393f31ebb9d00ebfeb9
William Odell <wodell@liveperson.com>
2020-04-24 15:13:19 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("This line will remain the same in all versions");
+
+console.log("This line will change in every version", 1);
+
+console.log("This line will be deleted in one version");
