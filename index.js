2acc398e3e0db43ce0ae58eba8b10190d933f7ac
William Odell <wodell@liveperson.com>
2020-05-11 11:56:53 +0100

    Dev commit 2

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ console.log("This line will remain the same in all versions");
 console.log("This line will change in every version", "developreleasetest2");
 
 console.log("Adding this new line in for develop release test");
+
+console.log("Adding this new line in for develop release test after release has branched off");
