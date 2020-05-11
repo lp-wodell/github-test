6625ed31c1c0f9168ffe6f9d466575d62cf726a2
William Odell <wodell@liveperson.com>
2020-05-11 12:01:03 +0100

    Final commit to dev after release merge

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ console.log("This line will remain the same in all versions");
 console.log("This line will change in every version", "developreleasetest-release");
 
 console.log("This replaces every line as part of merge from dev > master");
+
+console.log("Adding this line to dev after release merge");
